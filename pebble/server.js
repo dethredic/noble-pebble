802f6cbf25e0bdab92a6ef59d18bc2bca7a64453
Mario Carranca <mario@pebble.com>
2016-03-11 16:40:33 -0500

    Basic form

--- pebble/server.js
+++ pebble/server.js
@@ -1,6 +1,10 @@
 
 const Hapi = require('hapi');
 const hapiServer = new Hapi.Server();
+const ANCSNotifBuilder = require('./ancs_notif_builder');
+var notif_builder = new ANCSNotifBuilder();
+
+var lastUid = 0;
 
 var server =  function() {
     hapiServer.connection({
@@ -18,7 +22,40 @@ var server =  function() {
             method: 'GET',
             path: '/ancs',
             handler: function (request, reply) {
-                reply.file('./public/hello.html');
+                reply.file('./pebble/view/ancs_form.html');
+            }
+        });
+
+        hapiServer.route({
+            method: 'POST',
+            path: '/ancs',
+            handler: function (request, reply) {
+                var notificationUID = lastUid++;
+                var notification = notif_builder.create_ns_notification(notificationUID);
+
+                var payload = request.payload;
+                var appIdentifier = new Buffer(payload['appIdentifier']);
+                var title = new Buffer(payload['title']);
+                var subtitle = new Buffer(payload['subtitle']);
+                var message = new Buffer(payload['message']);
+                var date = new Buffer(new Date(payload['date']).toISOString());
+
+
+                var notificationAttributes = notif_builder.create_notif_attribtues(
+                    notificationUID, 
+                    appIdentifier, 
+                    title, 
+                    subtitle, 
+                    message, 
+                    date
+                );
+
+                var displayName = new Buffer(payload['appName']);
+                var appAttributes = notif_builder.create_app_attributes(appIdentifier, displayName);
+
+                ancs.send_notification(notification, notificationAttributes, appAttributes);
+
+                reply.file('./pebble/view/ancs_form.html');
             }
         });
     });
@@ -28,7 +65,7 @@ var server =  function() {
             return;
         }
 
-        console.log('hapiServer running at:', hapiServer.info.uri);
+        console.log('Open ' + hapiServer.info.uri + '/ancs to use Noble Pebble.');
     });
 };
 
